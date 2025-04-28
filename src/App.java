378c25e4223a2dec76faf913392b438134495416
Lucas Boaventura <lucasxboaventura@hotmail.com>
2025-04-28 13:06:13 -0300

    Altera main para fazer a soma de dois inteiros

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a = 6, b = 7;
+        int soma = a + b;
+        System.out.println(soma);
     }
 }
