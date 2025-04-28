df7d4176cabfcdb5c815353e41bf6444d599ace7
Lucas Boaventura <lucasxboaventura@hotmail.com>
2025-04-28 13:15:17 -0300

    instacia classe professor na main

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int a = 6, b = 7;
-        int soma = a + b;
-        System.out.println(soma);
+        Professor professor = new Professor(83219038);
     }
 }
